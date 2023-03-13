6ca8d6e8fbd0a92658161ac20a9cb21616c10866
Hieu Cu <hieu.dcu@gmail.com>
2023-03-13 21:53:59 +1300

    Removed wrong move array. Changed variables names.

--- script.js
+++ script.js
@@ -8,20 +8,15 @@ const winList = [
     [1, 4, 7],
     [2, 5, 8],
 ]
-const dumbMove = [
-    [1, 3, 5],
-    [1, 3, 7],
-    [3, 5, 7],
-];
 let array = [0,1,2,3,4,5,6,7,8];
-let newArray = [];
-let difference;
+let takenArray = [];
+let availableArray = [];
 let playerArray = [];
 let inputPlayer1 = "X";
 let inputPlayer2 = "O";
 let move;
 let getID;
-let random;
+let firstMove;
 let winningChance;
 let unmarked;
 let marked;
@@ -31,28 +26,28 @@ playerTurn.forEach(choice  => {
         choice.textContent = inputPlayer1;
             getID = event.target.getAttribute('id');
             playerArray.push(Number(getID));
-            newArray.push(Number(getID));
-            difference = array.filter(x => !newArray.includes(x));
+            takenArray.push(Number(getID));
+            availableArray = array.filter(x => !takenArray.includes(x));
             // computer's move (after player's click)
-            blockPlayer();
-            console.log('newArray: ' +newArray);
-            if (playerArray.length < 2) {
-                setTimeout(function(){ //delay.
-                mathRandom();
-                move = document.getElementById(random);
-                        move.textContent = inputPlayer2;
-                    },1000); //delay is in milliseconds 
-            }
+                blockPlayer();
+                console.log('takenArray: ' +takenArray);
+                    if (playerArray.length < 2) {
+                        mathRandom();
+                    };
+                console.log('player array: ' + playerArray);
+                console.log('takenArray: ' + takenArray);
+                console.log('available: '+ availableArray);
         });
     });
 function mathRandom() {
+    setTimeout(function(){ //delay.
     document.querySelectorAll('button');
-    random = difference[Math.floor(Math.random()*difference.length)];
-    newArray.push(random);
-        if (playerArray.length > 2) {
-            playerArray.splice(0, 1);
-        }
-}
+    firstMove = availableArray[Math.floor(Math.random()*availableArray.length)];
+    move = document.getElementById(firstMove);
+    move.textContent = inputPlayer2;
+    takenArray.push(Number(firstMove));
+},1000); //delay is in milliseconds 
+};
 //function to block winning moves.
 function blockPlayer() {
     for (let i = 0; i < winList.length; i++) {
@@ -60,30 +55,16 @@ function blockPlayer() {
          marked = combo.filter(cell => playerArray.includes(cell));
         if (marked.length === 2) {
             unmarked = combo.filter(cell => !playerArray.includes(cell));
-            if (difference.includes(unmarked[0])) {
-                newArray.push(unmarked[0]);
+            takenArray.push(marked[0]);
+            if (availableArray.includes(unmarked[0])) {
+                takenArray.push(unmarked[0]);
                 move = document.getElementById(unmarked);
                 setTimeout(function(){//delay.
                 move.textContent = inputPlayer2;
                 return unmarked[0];
             },1000); //delay is in milliseconds 
-            }
-        }
-    }
-    for (let j = 0; j < dumbMove.length; j++) {
-        let dumb = dumbMove[j];
-        let wrongMove = dumb.filter(cell => playerArray.includes(cell));
-        if (wrongMove.length === 2) {
-            let wrong = dumb.filter(cell => !playerArray.includes(cell));
-            if (difference.includes(wrong[0])) {
-                newArray.push(wrong[0]);
-                move = document.getElementById(wrong);
-                setTimeout(function(){//delay.
-                move.textContent = inputPlayer2;
-                return wrong[0];
-                },1000); //delay is in milliseconds 
-            }
-        }
-    }
-    return null;
-}
+            };
+        };
+    } ;
+};
+//determine winner/loser
