8065d1c0a5624c506d2587213ac699bd502f88f4
Hieu Cu <hieu.dcu@gmail.com>
2023-03-14 14:33:53 +1300

    Added random choice function if 2 possibilities of winning

--- script.js
+++ script.js
@@ -18,8 +18,7 @@ let move;
 let getID;
 let firstMove;
 let winningChance;
-let unmarked;
-let marked;
+let odd;
 const playerTurn = document.querySelectorAll('button');
 playerTurn.forEach(choice  => {
      choice.addEventListener('click', event => {
@@ -28,17 +27,16 @@ playerTurn.forEach(choice  => {
             playerArray.push(Number(getID));
             takenArray.push(Number(getID));
             availableArray = array.filter(x => !takenArray.includes(x));
+            console.log('player array: ' + playerArray);
             // computer's move (after player's click)
-                blockPlayer();
                 console.log('takenArray: ' +takenArray);
                     if (playerArray.length < 2) {
                         mathRandom();
                     };
-                console.log('player array: ' + playerArray);
-                console.log('takenArray: ' + takenArray);
-                console.log('available: '+ availableArray);
+                    blockPlayer();
         });
     });
+    console.log('takenArray: ' + takenArray);
 function mathRandom() {
     setTimeout(function(){ //delay.
     document.querySelectorAll('button');
@@ -50,21 +48,50 @@ function mathRandom() {
 };
 //function to block winning moves.
 function blockPlayer() {
+    availableArray = array.filter(x => !takenArray.includes(x));
     for (let i = 0; i < winList.length; i++) {
         let combo = winList[i];
-         marked = combo.filter(cell => playerArray.includes(cell));
+        let marked = combo.filter(cell => playerArray.includes(cell));
         if (marked.length === 2) {
-            unmarked = combo.filter(cell => !playerArray.includes(cell));
-            takenArray.push(marked[0]);
+           let unmarked = combo.filter(cell => !playerArray.includes(cell));
             if (availableArray.includes(unmarked[0])) {
                 takenArray.push(unmarked[0]);
-                move = document.getElementById(unmarked);
+               let move = document.getElementById(unmarked);
                 setTimeout(function(){//delay.
                 move.textContent = inputPlayer2;
-                return unmarked[0];
-            },1000); //delay is in milliseconds 
+                console.log('available: '+ availableArray);
+                },1000); //delay is in milliseconds 
+                    return unmarked[0];
+            }
+            //Check if there are 2 winning possibilities.
+            else if (availableArray.includes(unmarked[1])) {
+                takenArray.push(unmarked[1]);
+                let move = document.getElementById(unmarked);
+                setTimeout(function(){//delay.
+                move.textContent = inputPlayer2;
+                console.log('available: '+ availableArray);
+                },1000); //delay is in milliseconds 
+                    return unmarked[1];
+            }
+            //if there are, choose a single random outcome.
+            else if (unmarked.length > 1) {
+                let randomIndex = Math.floor(mathRandom()*unmarked.length);
+                let randomCell = unmarked[randomIndex];
+                takenArray.push(randomCell);
+                let move = document.getElementById(randomCell);
+                setTimeout(function(){//delay.
+                move.textContent = inputPlayer2;
+                console.log('available: '+ availableArray);
+                },1000); //delay is in milliseconds 
+                    return unmarked[randonIndex];
             };
-        };
-    } ;
+        };        
+            if (marked.length === 3) {
+                setTimeout(function(){//delay.
+                alert('You won!');
+                },1000); //delay is in milliseconds
+            }
+    };
 };
+
 //determine winner/loser
